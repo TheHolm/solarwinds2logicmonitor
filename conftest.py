c3ec32bdfbd69cdf8cb362bc3a13aa1e0ca7eaf9
TheHolm <16282750+TheHolm@users.noreply.github.com>
2020-11-12 15:01:27 +1100

    Minore refactor of ./conftest.py

--- conftest.py
+++ conftest.py
@@ -3,11 +3,22 @@ import configparser
 import json
 
 
-@pytest.fixture(scope="session")
-def sandbox_login_details():
+def __copy_from_config_ini(config_section, keys_to_copy):
     config = configparser.ConfigParser(interpolation=None)
     config.read("config.ini")
-    login_details = {'access_id': json.loads(config['LogicMonitor']['access_id']),
-                     'access_key': json.loads(config['LogicMonitor']['access_key']),
-                     'company': json.loads(config['LogicMonitor']['company'])}
-    return login_details
+
+    data = {}
+    for key in keys_to_copy:
+        data[key] = json.loads(config[config_section][key])
+    return data
+
+
+@pytest.fixture(scope="session")
+def sandbox_login_details():
+    ''' Coping data from INI file is intentially manual to avoid "silent" adding of paramiters. '''
+    return __copy_from_config_ini('LogicMonitor', ('access_id', 'access_key', 'company'))
+
+
+@pytest.fixture(scope="session")
+def file_backend_details():
+    return __copy_from_config_ini('FileTreeBackend', ('path', ))
