8f79aa9c920a2a7b427fba386c062e819efc4c98
TheHolm <16282750+TheHolm@users.noreply.github.com>
2021-01-13 12:04:19 +1100

    Added API-testing.py

---
+++ api-testing.py
@@ -0,0 +1,32 @@
+import json
+import configparser
+
+import lm_backend
+
+import pprint
+
+# main()
+
+pp = pprint.PrettyPrinter(indent=2)
+
+# calculate_LM_ini_customProperties()
+
+config = configparser.ConfigParser(interpolation=None)
+
+config.read("config.ini")
+
+api_instance = lm_backend.API_Session(json.loads(config['LogicMonitor']['access_id']),
+                                      json.loads(config['LogicMonitor']['access_key']),
+                                      json.loads(config['LogicMonitor']['company']))
+
+
+payload = {}
+payload['customProperties'] = []
+result = None
+
+result = api_instance.get('/device/groups/1880' + '', params={'v': 2})
+# result = api_instance.get('/device/devices/3669' + '')
+
+pp.pprint(result)
+
+quit()
